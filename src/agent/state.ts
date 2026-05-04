116b14dbe41e0f06cad43941411d937e7e4dc033
zzy <1580574280@qq.com>
2026-05-04 16:04:26 +0800

    feat: define AgentState with messages, collectedInfo, phase

---
+++ src/agent/state.ts
@@ -0,0 +1,21 @@
+// src/agent/state.ts
+import { Annotation } from "@langchain/langgraph";
+import { BaseMessage } from "@langchain/core/messages";
+import type { CollectedInfo } from "../schemas/collected-info";
+
+export const AgentState = Annotation.Root({
+  messages: Annotation<BaseMessage[]>({
+    reducer: (x, y) => x.concat(y),
+    default: () => [],
+  }),
+  collectedInfo: Annotation<CollectedInfo>({
+    reducer: (_, newVal) => newVal,
+    default: () => ({ preferences: [], constraints: [] }),
+  }),
+  phase: Annotation<string>({
+    reducer: (_, newVal) => newVal,
+    default: () => "info_gathering",
+  }),
+});
+
+export type AgentStateType = typeof AgentState.State;
